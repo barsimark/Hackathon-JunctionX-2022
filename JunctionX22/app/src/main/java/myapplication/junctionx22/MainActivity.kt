425081106e4c3efb8ee3901b13831f5e9120a10b
barsimark <barsimark@gmail.com>
2022-10-22 08:07:20 +0200

    buttons to layout

--- JunctionX22/app/src/main/java/myapplication/junctionx22/MainActivity.kt
+++ JunctionX22/app/src/main/java/myapplication/junctionx22/MainActivity.kt
@@ -2,10 +2,22 @@ package myapplication.junctionx22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import myapplication.junctionx22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.btnExistingJar.setOnClickListener {
+            Toast.makeText(this, "Existing jar button", Toast.LENGTH_LONG).show()
+        }
+        binding.btnAddNewJar.setOnClickListener {
+            Toast.makeText(this, "Add new jar button", Toast.LENGTH_LONG).show()
+        }
     }
 }

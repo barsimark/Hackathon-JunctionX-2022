ddf19d1f669f2fce654d1f72ed87f30c3e0b4473
barsimark <barsimark@gmail.com>
2022-10-22 15:46:35 +0200

    animations

--- JunctionX22/app/src/main/java/myapplication/junctionx22/jar/JarActivity.kt
+++ JunctionX22/app/src/main/java/myapplication/junctionx22/jar/JarActivity.kt
@@ -1,17 +1,22 @@
 package myapplication.junctionx22.jar
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import myapplication.junctionx22.R
 import myapplication.junctionx22.databinding.ActivityJarBinding
 import myapplication.junctionx22.model.JarItem
 import myapplication.junctionx22.model.JarItemManager
+import kotlin.concurrent.thread
+
 
 class JarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityJarBinding
     private lateinit var adapter: JarAdapter
+
+    private var animate = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +30,7 @@ class JarActivity : AppCompatActivity() {
         binding.btnWithdraw.setOnClickListener {
             JarItemManager.addItem(JarItem("Withdrawal", -2000))
             adapter.updateAll(JarItemManager.getItems())
+            animateDown()
         }
     }
 
@@ -32,13 +38,70 @@ class JarActivity : AppCompatActivity() {
         super.onResume()
 
         initRecyclerView()
+        animateUp()
     }
 
-    fun initRecyclerView(){
+    private fun initRecyclerView(){
         adapter = JarAdapter()
         binding.recyclerview.layoutManager = LinearLayoutManager(this)
         binding.recyclerview.adapter = adapter
 
         adapter.updateAll(JarItemManager.getItems())
+    }
+
+    private fun animateUp(){
+        loadGifUp(animate)
+        thread {
+            Thread.sleep(1700)
+            runOnUiThread(){
+                loadGifUp(animate)
+            }
+        }
+    }
+
+    private fun animateDown(){
+        loadGifDown(animate)
+        thread {
+            Thread.sleep(1500)
+            runOnUiThread(){
+                loadGifDown(animate)
+            }
+        }
+    }
+
+    private fun loadGifUp(toAnimate: Boolean){
+        animate = if (toAnimate){
+            Glide.with(this)
+                .asGif()
+                .load(R.raw.tree_from_zero_to_max)
+                .into(binding.gifImageView)
+
+            false
+        } else {
+            Glide.with(this)
+                .asGif()
+                .load(R.raw.full_tree)
+                .into(binding.gifImageView)
+
+            true
+        }
+    }
+
+    private fun loadGifDown(toAnimate: Boolean){
+        animate = if (toAnimate){
+            Glide.with(this)
+                .asGif()
+                .load(R.raw.tree_from_max_back_to_zero)
+                .into(binding.gifImageView)
+
+            false
+        } else {
+            Glide.with(this)
+                .asGif()
+                .load(R.raw.zero_tree)
+                .into(binding.gifImageView)
+
+            true
+        }
     }
 }
